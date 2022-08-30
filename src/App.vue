89dc784549439007ffd6121075d633bc4841f2e2
陈晶丹 <2972463088@qq.com>
2022-08-30 20:32:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo="addTodo"></MyHeader>
+        <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"></MyList>
+        <MyFooter :todos="todos"></MyFooter>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyHeader from "./components/MyHeader.vue";
+import MyList from "./components/MyList.vue";
+import ListItem from "./components/ListItem.vue";
+import MyFooter from "./components/MyFooter.vue";
+export default {
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "吃饭", done: true },
+        { id: "002", title: "睡觉", done: false },
+        { id: "003", title: "写代码", done: true },
+        { id: "004", title: "看电视", done: false },
+      ],
+    };
+  },
+  components: { MyHeader, MyList, ListItem, MyFooter },
+  methods: {
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj);
+    },
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        // console.log(todo);   //遍历的每一项todo
+        if (todo.id === id) {
+          // 遍历的某一项的id = 点击的id 那么就取反
+          return (todo.done = !todo.done);
+        }
+      });
+    },
+    deleteTodo(id) {
+      // 由于filter不改变data中的原数组，需要把过滤出来的新数组 = 赋值给原数组 
+      this.todos = this.todos.filter((todo)=>{
+        // 过滤出来的数组的条件
+        return todo.id !== id // 不是我删除的的那个id被过滤下来
+      }
+      )
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="todo-header">
+    <input
+      type="text"
+      placeholder="请输入你的任务名称，按回车键确认"
+      v-model="title"
+      @keyup.enter="add"
+    />
+  </div>
+</template>
+
+<script>
+import { nanoid } from "nanoid";
+
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+  props: ["addTodo"],
+  methods: {
+    // add(e) {
+    //   const todoObj = { id: nanoid(), title: e.target.value, done: false };
+    //   console.log(todoObj); // 添加的对象
+    //   this.addTodo(todoObj)  // 调用父传过来的方法 ，并且把父需要的数据作为参数传过去
+    //   e.target.value = ''
+    // },
+    add() {
+      if (!this.title.trim()) {
+        return alert("输入不能为空");
+      }
+      const todoObj = { id: nanoid(), title: this.title, done: false };
+      this.addTodo(todoObj);
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*header*/
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
+
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
+    0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
